d9d4e6a5cf722a76df7e0fac35bcb097ed24ecee
saeedhaider238 <saeed.haider@burntbrains.com>
2025-12-17 17:10:45 +0500

    deployment vercel try

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Routes, Route, Navigate } from "react-router";
-import Home from './pages/home/home';
+import Home from './pages/home/Home';
 
 const Router = () => {
   return (
